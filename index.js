cdd5d0dfc65eae92677ff17787120a47c6bb6a28
Rick <ricksmycodeguy@gmail.com>
2021-09-30 12:46:42 -0500

    bring in react

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
-console.log('Rick Rules');
+const path = require('path')
+// console.log('Rick Rules');
 console.log(process.argv[2]);
-console.log(process.argv[3]);
+// console.log(process.argv[3]);
 console.log(process.env.USER)
+// console.log(process.env.FOO);
 
 
 const express = require('express')
@@ -10,12 +12,21 @@ const express = require('express')
 const server = express()
 
 server.use(express.json())
+server.use(express.static(
+    path.join(__dirname, 'client/build')
+    ))
+server.get('*', (req, res) => {
+    res.sendFile(
+        path.join(__dirname, 'client/build', 'index.html')
+    )
+})
 
-const port = process.env.PORT;
+    server.get('/api/users', (req, res) =>{
+        res.send('<h1>Hello There!</h2>')
+    })
+
+const port = process.env.PORT || 3000;
+
 server.listen(port, () => {
     console.log('Listening on PORT:', port);
-})
-
-server.get('/hello', (req, res) =>{
-    res.send('<h1>Hello There!</h2>')
-})
+});
